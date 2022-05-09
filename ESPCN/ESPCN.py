9938a471bcb4ad51065012bc3f248b9a752ae66a
jl3419-ljy <jl3419@ic.ac.uk>
2022-05-09 14:56:22 +0100

    4 methods with .prof files for python profiling

---
+++ ESPCN/ESPCN.py
@@ -0,0 +1,23 @@
+import cv2
+import matplotlib.pyplot as plt
+
+img = cv2.imread("Test-image.png")
+print(cv2.__version__)
+sr = cv2.dnn_superres.DnnSuperResImpl_create()
+path = "ESPCN_x3.pb"
+sr.readModel(path)
+sr.setModel("espcn",3)
+result = sr.upsample(img)
+# Resized image
+resized = cv2.resize(img,dsize=None,fx=3,fy=3)
+plt.figure(figsize=(12,8))
+plt.subplot(1,3,1)
+# Original image
+plt.imshow(img[:,:,::-1])
+plt.subplot(1,3,2)
+# SR upscaled
+plt.imshow(result[:,:,::-1])
+plt.subplot(1,3,3)
+# OpenCV upscaled
+plt.imshow(resized[:,:,::-1])
+plt.show()
