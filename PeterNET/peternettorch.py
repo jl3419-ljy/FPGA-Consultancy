b05854abe4837f89b912f8d2891c95b048c7acdd
Terrortorpe <barabas.peter.akos@gmail.com>
2022-06-09 17:18:40 +0100

    asd

--- PeterNET/peternettorch.py
+++ PeterNET/peternettorch.py
@@ -1,3 +1,4 @@
+import torch.onnx
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
@@ -21,7 +22,6 @@ test_data = datasets.FashionMNIST(
 )
 
 batch_size = 64
-bit_width = 3
 
 # Create data loaders.
 train_dataloader = DataLoader(training_data, batch_size=batch_size)
@@ -101,6 +101,33 @@ def test(dataloader, model, loss_fn):
         f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
 
+# Function to Convert to ONNX
+
+
+def Convert_ONNX():
+
+    # set the model to inference mode
+    model.eval()
+
+    # Let's create a dummy input tensor
+    dummy_input = torch.randn(1, 28*28, requires_grad=True)
+
+    # Export the model
+    torch.onnx.export(model,         # model being run
+                      # model input (or a tuple for multiple inputs)
+                      dummy_input,
+                      "ImageClassifier.onnx",       # where to save the model
+                      export_params=True,  # store the trained parameter weights inside the model file
+                      opset_version=10,    # the ONNX version to export the model to
+                      do_constant_folding=True,  # whether to execute constant folding for optimization
+                      input_names=['modelInput'],   # the model's input names
+                      output_names=['modelOutput'],  # the model's output names
+                      dynamic_axes={'modelInput': {0: 'batch_size'},    # variable length axes
+                                    'modelOutput': {0: 'batch_size'}})
+    print(" ")
+    print('Model has been converted to ONNX')
+
+
 epochs = 5
 for t in range(epochs):
     print(f"Epoch {t+1}\n-------------------------------")
@@ -108,8 +135,8 @@ for t in range(epochs):
     #test(test_dataloader, model, loss_fn)
 print("Done!")
 
-torch.save(model.state_dict(), "model.pth")
+torch.save(model.state_dict(), "model2.pth")
 print("Saved PyTorch Model State to model.pth")
 
-ONNX = NeuralNetwork()
+Convert_ONNX() 
 
